0ad75565fe41dd721c1a8abb3e0453c0ca343599
Naitik <nkotharin558@gmail.com>
2025-08-07 01:06:54 -0400

    stream routes fixed

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import z from "zod";
 import { prismaClient } from "@/lib/db";
-const YT_REGEX = new RegExp("^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]{11})$");
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api"
 
+var YT_REGEX = /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/
 
 const CreateStreamSchema = z.object({
     creatorId: z.string(),
@@ -17,7 +19,7 @@ const CreateStreamSchema = z.object({
 export async function POST(req: NextRequest){
     try{
         const data = CreateStreamSchema.parse(await req.json())
-        const isYt = YT_REGEX.test(data.url);
+        const isYt = data.url.match(YT_REGEX);
         if(!isYt){
             return NextResponse.json({message:"Invalid URL! Only YouTube URLs are allowed."},{
                 status: 400,
@@ -26,16 +28,35 @@ export async function POST(req: NextRequest){
         }
 
         const extractedId = data.url.split("?v=")[1];
-        await prismaClient.stream.create({
+
+        const res = await youtubesearchapi.GetVideoDetails(extractedId);
+        const thumbnails = res.thumbnail.thumbnails
+        const thumbnail = thumbnails.sort((a: { width: number }, b: { width: number }) =>
+      a.width < b.width ? -1 : 1,
+    );
+         
+
+        const stream = await prismaClient.stream.create({
             data : {
                 userId: data.creatorId,
                 url: data.url,
                 extractedId,
                 type: "Youtube",
-                upvotes: 0
+                upvotes: 0,
+                title: res.title,
+                smallImg: (thumbnails.length > 1
+                ? thumbnails[thumbnails.length - 2].url
+                : thumbnails[thumbnails.length - 1].url) ??
+                "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg",
+                bigImg:thumbnails[thumbnails.length - 1].url ??
+                "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg"
             }
         })
+        return NextResponse.json({message:"Stream Added!",
+            id:stream.id
+        },{status:200})
     } catch (error) {
+        console.log(error)
         return NextResponse.json({message:"Invalid Request.....Try Again!"},{
             status: 400,
             statusText: "Bad Request"
@@ -45,9 +66,9 @@ export async function POST(req: NextRequest){
 
 export async function GET(req: NextRequest){
     const creatorId = req.nextUrl.searchParams.get("creatorId")
-    const streams = await prismaClient.user.findMany({
+    const streams = await prismaClient.stream.findMany({
         where: {
-            id: creatorId ?? undefined
+            userId: creatorId ?? undefined
         }
     })
     return NextResponse.json({streams})
